885275d871b35fc059e547a4ff6625e051e52191
Tim Molteno <tim@physics.otago.ac.nz>
2015-01-30 09:56:27 +1300

    Working cross-platform case conversion. Will be replaced by grammar based parser in future

--- src/c_geometry.cpp
+++ src/c_geometry.cpp
@@ -102,8 +102,6 @@ void str_toupper(std::string &str)
 		str.end(), 
 		str.begin(),
 		::toupper);
-//	[](char c) { return (toupper(c)); });
-	//		(int(*)(int)) std::toupper);
 }
 void c_geometry::parse_geometry(nec_context* in_context, FILE* input_fp )
 {
