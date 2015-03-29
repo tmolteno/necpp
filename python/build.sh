9ac5cc492dff6d484c6c154791ad986c4b3e77c5
Tim Molteno <tim@physics.otago.ac.nz>
2015-03-30 12:38:42 +1300

    Getting ready for a PyPI package

--- python/build.sh
+++ python/build.sh
@@ -2,6 +2,7 @@
 # Script to build the nec2++ ruby module.
 # Change the RUBY environment variable 
 # as appropriate for your system
+../configure --without-lapack
 PYTHON=python
 swig -v -I/usr/local/include -python necpp.i
 sudo python setup.py install
