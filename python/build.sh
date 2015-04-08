aab08193fffa990c9cd5874a03bf5917f2526a2b
Tim Molteno <tim@molteno.net>
2015-04-09 08:57:39 +1200

    Moved to python-necpp repository

--- python/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Script to build the nec2++ ruby module.
-# Change the RUBY environment variable 
-# as appropriate for your system
-../configure --without-lapack
-PYTHON=python
-swig -v -I/usr/local/include -python necpp.i
-sudo python setup.py install
