eec5208deac6b5ef061936d9fe76d9bc54501907
Tim Molteno <tim@physics.otago.ac.nz>
2015-05-12 13:00:59 +1200

    Move to single parameter geometry_complete() function

--- example/test.py
+++ example/test.py
@@ -17,7 +17,7 @@ def frequency_response():
   for f in range(1,30):
     nec = necpp.nec_create()
     handle_nec(necpp.nec_wire(nec, 1, 17, 0, 0, 2, 0, 0, 11, 0.1, 1, 1))
-    handle_nec(necpp.nec_geometry_complete(nec, 1, 0))
+    handle_nec(necpp.nec_geometry_complete(nec, 1))
     handle_nec(necpp.nec_gn_card(nec, 1, 0, 0, 0, 0, 0, 0, 0))
     handle_nec(necpp.nec_fr_card(nec, 0, 1, f, 0))
     handle_nec(necpp.nec_ex_card(nec, 0, 0, 5, 0, 1.0, 0, 0, 0, 0, 0))
