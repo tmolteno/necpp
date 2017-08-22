fa7fc22ec288a5868303158033b0612afd0e1d3c
Tim Molteno <tim@physics.otago.ac.nz>
2017-08-22 14:57:55 +1200

    Updated testbench for bessel functions.

--- src/c_evlcom_tb.cpp
+++ src/c_evlcom_tb.cpp
@@ -12,14 +12,21 @@ void test_bessel(nec_float r, nec_float i, nec_float j0r, nec_float j0i) {
      
      bessel( z, &j0, &j0p );
      
-     nec_float eps = 1.0e-4;
+     nec_float eps = std::fabs(j0r*1.0e-4);
+
      REQUIRE((std::fabs(j0.real() -  j0r) < eps));
      REQUIRE((std::fabs(j0.imag() -  j0i) < eps));
 }
 
+/** Generated with octave:
+ * besselj(0, 1j, 0)
+ * besselj(0, 1 + 1j, 0)
+ * besselj(0, 10j, 0)
+ * besselj(0, 10 + 10j, 0)
+ */
 TEST_CASE( "Bessel Functions", "[bessel]") {
      test_bessel(0, 1, 1.2661, 0);
-     test_bessel(1, 1, 1.2661, -0.4965);
-     test_bessel(0, 10, 2815.7, 0);
+     test_bessel(1, 1, 0.93761, -0.49653);
+     test_bessel(0, 10, 2815.71, 0);
      test_bessel(10, 10, -2314.98, 411.56);
 }
